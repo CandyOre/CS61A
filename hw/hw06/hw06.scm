6113dc676de640cc2ba27c851a2f5cfc2787f66e
CandyOreWin10 <834657460@qq.com>
2022-07-30 23:26:19 +0800

    Upd: Hw06 - Answer

--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -3,15 +3,22 @@
 
 (define (cadr s)
   'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
   'YOUR-CODE-HERE
+  (car (cddr s))
+  ; Alternative solution
+  ; (cadr (cdr s))
 )
 
 
 (define (sign num)
   'YOUR-CODE-HERE
+  (cond ((< num 0) -1)
+        ((> num 0) 1)
+        (else 0))
 )
 
 
@@ -19,5 +26,9 @@
 
 (define (pow x y)
   'YOUR-CODE-HERE
+  (if (= y 1) x
+    (if (= 1 (remainder y 2))
+      (* x (pow x (- y 1)))
+      (square (pow x (/ y 2)))))
 )
 
