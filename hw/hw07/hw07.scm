91b0a9d9c2a683c562bcf141ae3c536196700151
CandyOreWin10 <834657460@qq.com>
2022-07-30 23:26:30 +0800

    Upd: Hw07 - Answer

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,17 +1,26 @@
 (define (filter-lst fn lst)
   'YOUR-CODE-HERE
-)
+  (if (null? lst)
+      nil
+      (if (fn (car lst))
+          (cons (car lst) (filter-lst fn (cdr lst)))
+          (filter-lst fn (cdr lst)))))
 
 ;;; Tests
-(define (even? x)
-  (= (modulo x 2) 0))
+(define (even? x) (= (modulo x 2) 0))
+
 (filter-lst even? '(0 1 1 2 3 5 8))
 ; expect (0 2 8)
 
-
 (define (interleave first second)
   'YOUR-CODE-HERE
-)
+  (if (null? first)
+      second
+      (if (null? second)
+          first
+          (cons (car first)
+                (cons (car second)
+                      (interleave (cdr first) (cdr second)))))))
 
 (interleave (list 1 3 5) (list 2 4 6))
 ; expect (1 2 3 4 5 6)
@@ -25,10 +34,18 @@
 
 (define (accumulate combiner start n term)
   'YOUR-CODE-HERE
-)
-
+  (if (= n 0)
+      start
+      (combiner (term n)
+                (accumulate combiner start (- n 1) term))))
 
 (define (no-repeats lst)
   'YOUR-CODE-HERE
-)
-
+  (if (null? lst)
+      nil
+      (cons 
+        (car lst)
+        (no-repeats 
+          (filter-lst
+            (lambda (x) (not (= x (car lst))))
+            (cdr lst))))))
