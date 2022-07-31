7d67a2c3cd394a76cc0d3119c03763e5bf8f8c0b
CandyOreWin10 <834657460@qq.com>
2022-07-31 21:26:01 +0800

    Upd: Lab13 - Answer

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,17 +2,31 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, avg(MSRP) AS average_price
+  FROM products
+  GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, min(price) as price
+  FROM inventory
+  GROUP BY item;
+
+
+CREATE TABLE best_deal AS
+  SELECT min(MSRP / rating), category, name
+  FROM products
+  GROUP BY category;
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name, b.store
+  FROM best_deal AS a, lowest_prices AS b
+  WHERE a.name = b.item;
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT sum(b.Mbs)
+  FROM shopping_list AS a, stores as b
+  WHERE a.store = b.store;
 
